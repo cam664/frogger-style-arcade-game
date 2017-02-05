066aa71f724b66f34f74dd9e5f1caae36b5bb779
cam664 <c.laing@live.com>
2017-02-05 08:35:33 -0500

    remove some unecessary comments

--- js/app.js
+++ js/app.js
@@ -82,9 +82,7 @@ var Enemy = function(lane, speed, respawnNum) {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+
     this.x += this.speed * dt;
 };
 
@@ -102,9 +100,6 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 var Player = function() {
     this.sprite = 'images/char-empty.png';
     this.width = ENTITY_WIDTH;
@@ -185,10 +180,6 @@ Goal.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
 // Create a star and position randomly on a stone block
 var createStars = function() {
     var star = new Star(randomNum(0,2), Math.round(randomNum(0,400)/100)*100);
@@ -211,8 +202,8 @@ var player = new Player;
 
 var goal = new Goal;
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// This listens for key presses and sends the keys to
+// Player.handleInput() method.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
--- js/engine.js
+++ js/engine.js
@@ -235,9 +235,7 @@ var Engine = (function(global) {
 
     /**************************************************************************
      *
-     * This function does nothing but it could have been a good place to
-     * handle game reset states - maybe a new game menu or a game over screen
-     * those sorts of things. It's only called once by the init() method.
+     *
      * ***Game reset Handling***
      *
      **************************************************************************/
